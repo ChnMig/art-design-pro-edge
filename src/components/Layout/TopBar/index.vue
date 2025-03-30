c64d9cb5601e7184def4f5a02034a441bfe9e35c
chenming <chnmig@outlook.com>
2025-03-30 12:13:20 +0800

    feat: :sparkles: 修改用户信息显示，使用用户姓名替代用户名

--- src/components/Layout/TopBar/index.vue
+++ src/components/Layout/TopBar/index.vue
@@ -109,7 +109,7 @@
                 <div class="user-head">
                   <img class="cover" :src="userInfo.avatar" style="float: left" />
                   <div class="user-wrap">
-                    <span class="name">{{ userInfo.username }}</span>
+                    <span class="name">{{ userInfo.name }}</span>
                     <span class="email" v-if="userInfo.email">{{ userInfo.email }}</span>
                   </div>
                 </div>
